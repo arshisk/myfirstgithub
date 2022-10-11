9f144591234fe78a9be4bf551b05430cf61d6a5b
arshisk <115518176+arshisk@users.noreply.github.com>
2022-10-11 08:45:37 +0530

    Add files via upload
    just my new uploaded file of codes

---
+++ maths prac.R
@@ -0,0 +1,46 @@
+sin wave
+x=seq(0,2*pi,0.01)
+y=sin(x)
+plot(x,y,type="l")
+x
+
+cos wave
+x=seq(0,2*pi,0.01)
+y=cos(x)
+plot(x,y,type="l")
+x
+
+tan wave
+cos wave
+x=seq(0,2*pi,0.01)
+y=tan(x)
+plot(x,y,type="l")
+x
+
+absolute wave
+x=seq(-5,5,0.05)
+y=abs(x)
+plot(x,y,type="l")
+x
+
+ceiling wave
+x=seq(-5,5,0.05)
+y=ceiling(x)
+plot(x,y,type="l")
+
+floor wave
+x=seq(-5,5,0.05)
+y=floor(x)
+plot(x,y,type="l")
+
+logarithmic wave
+x=seq(10,100,5)
+y=log(x)
+plot(x,y,type="l")
+
+
+exponential wave
+x=seq(10,100,5)
+y=exp(x)
+plot(x,y,type="l")
+
